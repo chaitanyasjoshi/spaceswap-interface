e10a992879d21c03ef7d0586bf9f02fa8b445b5f
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2023-01-28 11:32:09 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DefaultTokenList, Token } from './app.constant';
+import { ContractService } from './services/contract.service';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+    title = 'spaceswap-interface';
+
+    swapData: any = {
+        fromToken: null,
+        toToken: null
+    };
+
+    myTokens: Array<Token>;
+
+    constructor(private _dialog: MatDialog, private _contractService: ContractService) {}
+
+    async ngOnInit() {
+        await this.loadTokenBalances();
+    }
+
+    async loadTokenBalances() {
+        this.myTokens = DefaultTokenList;
+        this.myTokens.forEach(async (token) => {
+            const balance = await this._contractService.getBalance(token);
+            token.balance = balance;
+        });
+
+        this.swapData.fromToken = this.myTokens[0];
+        this.swapData.toToken = this.myTokens[1];
+    }
+
+    async connect() {
+        const ethereum = (window as any)?.ethereum;
+
+        if (!ethereum?.isMetaMask) return null;
+        await ethereum.enable();
+
+        try {
+            // Will open the MetaMask UI
+            // You should disable this button while the request is pending!
+            await ethereum.request({ method: 'eth_requestAccounts' });
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+            if (accounts.length > 0) {
+                localStorage.setItem('spaceswap.wallet', accounts[0]);
+                // return true;
+            } else {
+                // return false;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        // return false;
+    }
+
+    async disconnect() {
+        localStorage.removeItem('spaceswap.wallet');
+        window.location.reload();
+    }
+}
