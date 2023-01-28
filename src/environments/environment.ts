5410662c42a0e7d0cc9002f088a6b2527502e2d2
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2023-01-28 15:04:08 +0530

    fix: env setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAIuH_Z32hR1QPlnX6PVx74Rb8JKUK4Fbo',
+        authDomain: 'spaceswap-3d8db.firebaseapp.com',
+        projectId: 'spaceswap-3d8db',
+        storageBucket: 'spaceswap-3d8db.appspot.com',
+        messagingSenderId: '773953513204',
+        appId: '1:773953513204:web:1b43024579edeb9e8a60a1'
+    }
+};
